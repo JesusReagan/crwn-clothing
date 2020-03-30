8cf893e7b91f6c8077b0afee7971d8af2618d019
Scott Ellis <ellis.lowell.scott@gmail.com>
2020-03-29 18:40:21 -0700

    Added redux, and implemented userReducer and userActions to header and app components

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,17 +3,41 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const config = {
-  apiKey: "AIzaSyAhilPlYfkixbB5bopmmhpWbn9n7p8Sf3U",
-  authDomain: "crwn-db-6abd7.firebaseapp.com",
-  databaseURL: "https://crwn-db-6abd7.firebaseio.com",
-  projectId: "crwn-db-6abd7",
-  storageBucket: "crwn-db-6abd7.appspot.com",
-  messagingSenderId: "613207537294",
-  appId: "1:613207537294:web:702f4ce97b1f73367a7bdf",
-  measurementId: "G-JGHTMHZNZ1"
+  apiKey: "AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14",
+  authDomain: "crwn-db.firebaseapp.com",
+  databaseURL: "https://crwn-db.firebaseio.com",
+  projectId: "crwn-db",
+  storageBucket: "crwn-db.appspot.com",
+  messagingSenderId: "850995411664",
+  appId: "1:850995411664:web:7ddc01d597846f65"
 };
 
 firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+
+  return userRef;
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
